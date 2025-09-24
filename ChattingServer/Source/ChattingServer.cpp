25b488af3bb1724ec18525795afc3c2099887094
flame-monkey <flamemonkeydev@outlook.com>
2025-09-25 05:16:59 +0900

    서버 구현 시작

---
+++ ChattingServer/Source/ChattingServer.cpp
@@ -0,0 +1,36 @@
+#include "ChattingServer.h"
+
+ChattingServer::ChattingServer()
+{
+
+}
+
+ChattingServer::~ChattingServer()
+{
+
+}
+
+void ChattingServer::Init()
+{
+
+}
+
+void ChattingServer::StartServer()
+{
+
+}
+
+void ChattingServer::Stop()
+{
+
+}
+
+void ChattingServer::Send()
+{
+
+}
+
+void ChattingServer::PrintStatus()
+{
+
+}
--- ChattingServer/Source/ChattingServer.h
+++ ChattingServer/Source/ChattingServer.h
@@ -1,6 +1,62 @@
-#pragma once
+﻿#pragma once
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Mswsock.lib")
+
+#include "StructMessage.h"
+#include "BufferManager.h"
+#include "SocketManager.h"
+
+class ChattingServer;
+
+enum ESocketOperation
+{
+	Accept = 0,
+	Recv = 1,
+	Send = 2,
+};
+
+struct SocketContext
+{
+	OVERLAPPED Overlapped;
+	ChattingServer* MyServer; // 이거 필요함?
+	ESocketOperation LastOp;
+	SOCKET Socket;
+	WSABUF* DataBuf;
+	int ImmediatelyReceivedBytes; // Won't use WSAGetOverlappedResult
+	int Flags;
+};
 
 class ChattingServer
 {
+private:
+	WSADATA WSAData;
+	HANDLE Completionport;
+	sockaddr_in SocketAddr;
+	short PortNum;
+	SOCKET ListenSocket;
+	SOCKET AcceptSocket;
+	SocketContext* AcceptContext;
 
+	static LPFN_ACCEPTEX lpfnAcceptEx;
+	static LPFN_GETACCEPTEXSOCKADDRS lpfnGetAcceptExSocketAddrs;
+
+	static DWORD WINAPI IOCPWorkerThread(LPVOID);
+
+	Message::BufferManager* MessageBufferManager;
+	unsigned long long MaxConnection;
+	SocketManager* SocketManagerPool; // array stack
+
+public:
+	ChattingServer();
+	~ChattingServer();
+
+	void Init();
+	void StartServer();
+	void Stop();
+	void Send();
+	void PrintStatus();
 };
